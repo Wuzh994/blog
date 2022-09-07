e601f4be8be359662f15b632a194e5e13e26f47e
wuzhenghong <2015129064@qq.com>
2022-09-07 18:31:11 +0800

    feat: ssg page-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
+import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
+import routes from '~pages'
 
-createApp(App).mount('#app')
+import '@unocss/reset/tailwind.css'
+import './styles/index.scss'
+import 'uno.css'
+
+export const createApp = ViteSSG(
+  App,
+  { routes },
+  // function to have custom setups
+  ({ app, router, routes, isClient, initialState }) => {
+    // install plugins etc.
+
+  },
+)
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import Components from 'unplugin-vue-components/vite'
+import Pages from 'vite-plugin-pages'
+import Markdown from 'vite-plugin-vue-markdown'
+import anchor from 'markdown-it-anchor'
+import prism from 'markdown-it-prism'
+import TOC from 'markdown-it-table-of-contents'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Unocss(),
+    Components({
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.md$/],
+      dirs: ['src/components', 'src/demo'],
+    }),
+    Markdown({
+      headEnabled: true,
+      markdownItSetup(md) {
+        md.use(anchor, {
+          slugify() {
+            return 'heading'
+          },
+        })
+        md.use(prism)
+        md.use(TOC, {
+          includeLevel: [1, 2, 3],
+          containerClass: 'article-catalog',
+          containerHeaderHtml: '<div class="catalog-title">目录</div>',
+        })
+      },
+    }),
+    Pages({
+      extensions: ['md'],
+      dirs: 'src/articles',
+    }),
+  ],
 })
